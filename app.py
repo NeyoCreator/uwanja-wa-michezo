f4fbaa19de44438d1ba2b5c7ee8b8524588e319c
AbutiWors1 <93925691+AbutiWors1@users.noreply.github.com>
2024-01-20 08:09:13 +0200

    Added basic backend and 3 html forms

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+import json
+import os
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'  # Change this to an actual secret key for production use
+
+def save_to_json(data):
+    file_path = 'data.json'
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            existing_data = json.load(file)
+        existing_data.append(data)
+        with open(file_path, 'w') as file:
+            json.dump(existing_data, file, indent=4)
+    else:
+        with open(file_path, 'w') as file:
+            json.dump([data], file, indent=4)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/consumption')
+def consumption():
+    return render_template('consumption.html')
+
+@app.route('/device')
+def device():
+    return render_template('device.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
