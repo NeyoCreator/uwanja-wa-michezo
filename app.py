3650825bbd8d158ebfe43824f5ba5068fed91858
AbutiWors1 <93925691+AbutiWors1@users.noreply.github.com>
2024-01-27 08:52:03 +0200

    Landing page content

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+from authlib.integrations.flask_client import OAuth
 import json
 import os
 from google.cloud import firestore
 from firebase_admin import credentials, firestore
-
 import firebase_admin
 
-app = Flask(__name__)
-app.secret_key = 'secret_data'  # Change this to an actual secret key for production use
 
-# database intergration
+# App initialisation
+app = Flask(__name__)
+app.secret_key = 'secret_data'
+
+# Database intergration
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
-
-# Database
 db = firestore.client()
-
-def save_to_json(data):
-    file_path = 'data.json'
-    if os.path.exists(file_path):
-        with open(file_path, 'r') as file:
-            existing_data = json.load(file)
-        existing_data.append(data)
-        with open(file_path, 'w') as file:
-            json.dump(existing_data, file, indent=4)
-    else:
-        with open(file_path, 'w') as file:
-            json.dump([data], file, indent=4)
 
 @app.route('/')
 def home():
@@ -42,7 +30,6 @@ def index():
 
         session['user_data'] = {'name': name, 'email': email, 'location': location}
         print(session)
-        # redirect(url_for('consumption'))
         return redirect(url_for('consumption'))
 
 
@@ -51,20 +38,16 @@ def index():
 @app.route('/consumption', methods = ['GET','POST'])
 def consumption():
     if request.method == 'POST':
-        # Retrieve data from the form
         selected_appliances = request.form.get('selectedAppliances')
         total_wattage = request.form.get('totalWatt')
 
-        # Do something with the data, for example, save it to a JSON file
         consumption_data = {
             'selected_appliances': selected_appliances.split(','),
             'total_wattage': total_wattage
         }
         session['consumption_data'] = consumption_data
         print(session)
-        # save_to_json(consumption_data)
 
-        # Redirect to the next page or perform other actions
         return redirect(url_for('device'))
     
     return render_template('consumption.html')
@@ -72,18 +55,14 @@ def consumption():
 @app.route('/device', methods=['GET', 'POST'])
 def device():
     if request.method == 'POST':
-        # Retrieve data from the form
         component_list = request.form.get('componentList')
 
-        # Do something with the data, for example, save it to a JSON file
         device_data = {
             'component_list': component_list.split(',')
         }
         session['device_data'] = device_data
         print("session",session)
-        # save_to_json(device_data)
 
-        # Redirect to the next page or perform other actions
         return redirect(url_for('installer'))
 
 
@@ -92,23 +71,19 @@ def device():
 @app.route('/installer',methods = ['GET','POST'])
 def installer():
     if request.method == 'POST':
-        # Retrieve data from the form
         installer_name = request.form.get('installer_name')
         installer_rating = request.form.get('installer_rating')
 
-        # Do something with the data, for example, save it to a JSON file
         installer_data = {
             'installer_name': installer_name,
             'installer_rating': installer_rating
         }
         session['installer_data'] = installer_data
-        # print("session", session)
 
         db.collection(session['user_data']['email']).document("data").set(session)
         print(session['user_data']['email'])
         
 
-        # Redirect to the next page or perform other actions
         return redirect(url_for('delivery'))
     return render_template('installer.html')
 
