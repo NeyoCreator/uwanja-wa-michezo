39faf961054e47f8159b66191ffbeb4ec4ddffb0
NeyoCreator <neos2722@gmail.com>
2024-04-26 22:25:22 -0700

    Updated user interface

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ auth = firebase.auth()
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
